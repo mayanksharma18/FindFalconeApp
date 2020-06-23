2bda98f76fb91110f971e2155ebf4db15713b085
Mayank Sharma <mmayank.sharma18@gmail.com>
2020-06-23 16:30:19 +0530

    Added redirect component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import GameStory from './Components/GameStory/GameStory';
 import PlayingScreen from './Components/PlayingScreen/PlayingScreen';
 import ResultScreen from './Components/ResultScreen/ResultScreen';
@@ -12,12 +12,14 @@ const App = () => {
     <div>
       <Router>
         <Header />
-        <Route exact path="/" component={GameStory} />
         <Route path="/gamestory" component={GameStory} />
         <ErrorBoundary>
           <Route path="/playgame" component={PlayingScreen} />{' '}
         </ErrorBoundary>
         <Route path="/results" component={ResultScreen} />
+        <Route path="/">
+          <Redirect to="/gamestory" />
+        </Route>
         <Footer />
       </Router>
     </div>
